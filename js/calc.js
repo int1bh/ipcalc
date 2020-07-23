16fa6cb8660a066d9fd281f95412ae9424656359
Alexandr <48558946+int1bh@users.noreply.github.com>
2020-07-23 17:14:09 +0300

    Закончен функционал расчета адресов

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let ip = '192.168.1.66',
-    mask = '255.255.255.128',
-    network = [];
+let button = document.querySelector('button');
+let ip, mask, network;
+
 
 function toBinary(item) {
     let arrayItem = item.split('.');
@@ -12,39 +12,45 @@ function toBinary(item) {
     return arrayElement;
 }
 
+
+button.addEventListener('click', () => {
+    event.preventDefault();
+    let prefix = parseInt(document.querySelector('select').value); //CIDR диапазон
+    let button = document.querySelector('button');
+    let resutl = document.querySelector('.result');
+
+    ip = document.querySelector('#ip').value,
+         mask = (document.querySelector('select').selectedOptions[0].text).slice(5, -1),
+         network = [];
+
+
+
+
 let ipArray = toBinary(ip),
     maskArray = toBinary(mask),
-    // maskLastOktet = parseInt(parseInt(maskArray[3], 2));
-    maskLastOktet = 25;
+    maskLastOktet = prefix;
 
-console.log(maskLastOktet);
 
-network[0] = parseInt(ipArray[0], 2) & parseInt(maskArray[0], 2);
-network[1] = parseInt(ipArray[1], 2) & parseInt(maskArray[1], 2);
-network[2] = parseInt(ipArray[2], 2) & parseInt(maskArray[2], 2);
-network[3] = parseInt(ipArray[3], 2) & parseInt(maskArray[3], 2);
+
+for (let i = 0; i < 4; i++) {
+    network.push(parseInt(ipArray[i], 2) & parseInt(maskArray[i], 2));
+}
+
 
 let totalHost = Math.pow(2, (32-maskLastOktet))-2;
-console.log(totalHost);
+
 
 let networkAddr = network.join(".");
 let firsAddr = network[3]+1;
 let lastAddr = network[3]+totalHost;
 
-console.log(`Адрес сети ${networkAddr}`);
-console.log(`Адрес минимально возможного хоста ${firsAddr}`);
-console.log(`Адрес максимально возможного хоста ${lastAddr}`);
 
-
-
-// console.log(Math.pow(2 , (32-29)));
-
-// console.log(ipArray);
-// console.log(maskArray);
-
-// let a = parseInt(11000000, 2);
-// let b = parseInt(11111111, 2);
-
-// let c = a & b;
-
-// console.log(c);
+    prefix = parseInt(document.querySelector('select').value);
+    resutl.innerHTML = `
+    <p>Адрес сети ${networkAddr}</p>
+    <br>
+    <p>Адрес минимально возможного хоста ${firsAddr}</p>
+    <br>
+    <p>Адрес максимально возможного хоста ${lastAddr}</p>
+    `;
+});
